84e9ce724a5396aa944e90f7b2db2c0bc3a24ee1
Digidai <daiq@live.cn>
2025-03-23 23:27:02 +0800

    Fix build errors and add TypeScript support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {},
   plugins: [],
 } 
