701e7fb901c1a2a8978c0eb6e8885126dfb6f821
Digidai <daiq@live.cn>
2025-03-23 23:11:58 +0800

    Further simplify project structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // 启用静态导出
-  images: {
-    unoptimized: true, // 为了静态导出
-  },
   basePath: '/genedai.me',
-  assetPrefix: '/genedai.me/',
 }
 
 export default nextConfig; 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
   theme: {},
   plugins: [],
-}
-
-export default config 
+} 
